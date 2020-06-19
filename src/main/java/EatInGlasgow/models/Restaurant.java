03bc330f031a6c998193a886e3ef50e660e21ff1
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 22:13:15 +0100

    create all the setters

--- src/main/java/EatInGlasgow/models/Restaurant.java
+++ src/main/java/EatInGlasgow/models/Restaurant.java
@@ -119,4 +119,52 @@ public class Restaurant {
     public String getClosingTime() {
         return closingTime;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setPriceRange(String priceRange) {
+        this.priceRange = priceRange;
+    }
+
+    public void setCousine(String cousine) {
+        this.cousine = cousine;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(String closingTime) {
+        this.closingTime = closingTime;
+    }
 }
