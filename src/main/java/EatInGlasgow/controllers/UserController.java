8980d18207c3ceb1e67f48bc69b35bd0eee9e850
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 23:04:24 +0100

    add getReviewById() to UserController

--- src/main/java/EatInGlasgow/controllers/UserController.java
+++ src/main/java/EatInGlasgow/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,8 @@ public class UserController {
         return new ResponseEntity<>(userRepo.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userRepo.findById(id), HttpStatus.OK);
+    }
 }
