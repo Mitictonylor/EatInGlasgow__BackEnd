de78eae03b775b95098300924609fa93d23c200a
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 22:32:40 +0100

    create ReviewRepository

---
+++ src/main/java/EatInGlasgow/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package EatInGlasgow.repositories;
+
+import EatInGlasgow.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+
+}
