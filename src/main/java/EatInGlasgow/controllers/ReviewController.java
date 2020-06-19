2e3ccb25981b714d0d67cc42604519a32d6c72d0
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 23:00:23 +0100

    add postReview() to ReviewController

--- src/main/java/EatInGlasgow/controllers/ReviewController.java
+++ src/main/java/EatInGlasgow/controllers/ReviewController.java
@@ -6,10 +6,7 @@ import EatInGlasgow.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,4 +27,9 @@ public class ReviewController {
         return new ResponseEntity<>(reviewRepo.findById(id), HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<Review> postReview(@RequestBody Review review){
+        reviewRepo.save(review);
+        return new ResponseEntity<>(review, HttpStatus.CREATED);
+    }
 }
