8cf272889217117ae991ba6e7b0e957fd2060d37
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 22:31:26 +0100

    create RestaurantRepository

---
+++ src/main/java/EatInGlasgow/repositories/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package EatInGlasgow.repositories;
+
+import EatInGlasgow.models.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
