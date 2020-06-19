8535b188122c34bcf1497aaee391f9edad041cb8
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 22:46:19 +0100

    add Crud on BookingController

--- src/main/java/EatInGlasgow/controllers/BookingController.java
+++ src/main/java/EatInGlasgow/controllers/BookingController.java
@@ -1,9 +1,13 @@
 package EatInGlasgow.controllers;
 
+import EatInGlasgow.models.Booking;
 import EatInGlasgow.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookings")
@@ -12,7 +16,29 @@ public class BookingController {
     @Autowired
     BookingRepository bookingRepository;
 
-    @
+    @GetMapping
+    public ResponseEntity<List<Booking>> getAllBookings(){
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Booking> postBooking(@RequestBody Booking booking){
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Booking> putBooking(@RequestBody Booking booking){
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Long> deleteBooking(@PathVariable Long id){
+        bookingRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
 
 
 
