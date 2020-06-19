a70161de4fd7ba32b482dbc676609fe76d0f0d33
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-19 22:30:12 +0100

    create BookingRepository

---
+++ src/main/java/EatInGlasgow/repositories/BookingRepository.java
@@ -0,0 +1,10 @@
+package EatInGlasgow.repositories;
+
+import EatInGlasgow.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+}
