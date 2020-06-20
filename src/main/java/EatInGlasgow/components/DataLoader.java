7ce2fe8922c1dad2069ed45e7cf1c1dc507048ad
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-20 10:34:25 +0100

    create DataLoader implementing application runner

---
+++ src/main/java/EatInGlasgow/components/DataLoader.java
@@ -0,0 +1,34 @@
+package EatInGlasgow.components;
+
+import EatInGlasgow.models.Review;
+import EatInGlasgow.repositories.BookingRepository;
+import EatInGlasgow.repositories.RestaurantRepository;
+import EatInGlasgow.repositories.ReviewRepository;
+import EatInGlasgow.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
