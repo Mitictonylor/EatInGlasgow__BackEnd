b4dbd18a9c78324b069500861dd51f551d5907ab
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-20 10:44:24 +0100

    add users

--- src/main/java/EatInGlasgow/components/DataLoader.java
+++ src/main/java/EatInGlasgow/components/DataLoader.java
@@ -1,6 +1,7 @@
 package EatInGlasgow.components;
 
 import EatInGlasgow.models.Review;
+import EatInGlasgow.models.User;
 import EatInGlasgow.repositories.BookingRepository;
 import EatInGlasgow.repositories.RestaurantRepository;
 import EatInGlasgow.repositories.ReviewRepository;
@@ -8,7 +9,11 @@ import EatInGlasgow.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -30,6 +35,25 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        User user1 = new User("Timi", "Kiraly", "timi@gmail.com", "Glasgow", "g512ng");
+        User user2 = new User ("Antonio", "Lorusso", "antonio@gmail.com", "Glasgow", "g512ng");
+        User user3 = new User ("Sarah", "Harrington", "sarah@gmail.com", "Glasgow", "g37tq");
+        User user4 = new User ("John", "Smith", "john@gmail.com", "Glasgow", "g311jd");
+        User user5 = new User ("Paul", "Robertson", "paul@gmail.com", "Paisley", "pa12de");
+        User user6 = new User ("Rachel", "Dawson", "rachel@gmail.com", "Glasgow", "g641ut");
+        User user7 = new User ("Eloise", "Summers", "eloise@gmail.com", "Glasgow", "g318se");
+        User user8 = new User ("Harry", "Brown", "harry@gmail.com", "Motherwell", "ml13dg");
+        User user9 = new User ("Michael", "Anderson", "michael@gmail.com", "Glasgow", "g414pg");
+        User user10 = new User ("Anna", "Darcy", "anna@gmail.com", "Glasgow", "g514tf");
+        User user11 = new User ("Robert", "Simons", "robert@gmail.com", "Glasgow", "g128qq");
+        User user12 = new User ("Lisa", "Wilson", "lisa@gmail.com", "Glasgow", "g38ag");
+        User user13 = new User ("Peter", "McDonald", "peter@gmail.com", "Glasgow", "g50an");
+        User user14 = new User ("Silvia", "Dickens", "silvia@gmail.com", "Glasgow", "g731ug");
+        User user15 = new User ("George", "Reynolds", "george@gmail.com", "Glasgow", "g58dj");
+
+
+
 
     }
 }
