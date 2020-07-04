03be4a9e5410b5d8477cef54a4e7018c8d3a7081
Antonio Lorusso <mitictonylor@gmail.com>
2020-07-04 15:21:44 +0100

    add search by email

--- src/main/java/EatInGlasgow/controllers/AdminController.java
+++ src/main/java/EatInGlasgow/controllers/AdminController.java
@@ -5,6 +5,7 @@ import com.okta.sdk.client.Client;
 import com.okta.sdk.resource.user.UserList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,11 @@ public class AdminController {
     @GetMapping("/users")
     public UserList getUsers() {
         return client.listUsers();
+    }
+
+    @GetMapping("/user")
+    public UserList searchUserByEmail(@RequestParam String query) {
+        return client.listUsers(query, null, null, null, null);
     }
 
 
