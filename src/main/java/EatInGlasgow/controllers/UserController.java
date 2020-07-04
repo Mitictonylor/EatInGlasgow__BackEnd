4b6e0016f1635db52eced7270b02c230e20f14f4
Antonio Lorusso <mitictonylor@gmail.com>
2020-07-04 11:57:35 +0100

    add userController for springSecurity and change the Usercontroller(table) to UsersController

--- src/main/java/EatInGlasgow/controllers/UserController.java
+++ src/main/java/EatInGlasgow/controllers/UserController.java
@@ -1,56 +1,48 @@
 package EatInGlasgow.controllers;
 
-import EatInGlasgow.models.User;
-import EatInGlasgow.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
+    private ClientRegistration registration;
 
-
-    private final Logger log = LoggerFactory.getLogger(UserController.class);
-    private UserRepository userRepo;
-
-    public UserController(UserRepository userRepo) {
-        this.userRepo = userRepo;
+    public UserController(ClientRegistrationRepository registrations) {
+        this.registration = registrations.findByRegistrationId("okta");
     }
 
-
-    @GetMapping(value = "/users")
-    public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<>(userRepo.findAll(), HttpStatus.OK);
+    @GetMapping("/api/user")
+    public ResponseEntity<?> getUser(@AuthenticationPrincipal OAuth2User user) {
+        if (user == null) {
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return ResponseEntity.ok().body(user.getAttributes());
+        }
     }
 
-    @GetMapping(value = "/users/{id}")
-    public ResponseEntity getUserById(@PathVariable Long id) {
-        return new ResponseEntity<>(userRepo.findById(id), HttpStatus.OK);
-    }
+    @PostMapping("/api/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request,
+                                    @AuthenticationPrincipal(expression = "idToken") OidcIdToken idToken) {
+        // send logout URL to client so they can initiate logout
+        String logoutUrl = this.registration.getProviderDetails()
+                .getConfigurationMetadata().get("end_session_endpoint").toString();
 
-    @PostMapping(value = "/users")
-    public ResponseEntity<User> postUser(@RequestBody User user) {
-        userRepo.save(user);
-        return new ResponseEntity<User>(user, HttpStatus.CREATED);
-    }
-
-
-    @PatchMapping(value = "/users/{id}")
-    public ResponseEntity<User> putUser( @RequestBody User user) {
-        userRepo.save(user);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-    @DeleteMapping(value = "/users/{id}")
-    public ResponseEntity deleteUser(@PathVariable Long id) {
-        userRepo.deleteById(id);
-        return new ResponseEntity<>("Deleted Object with id " + id, HttpStatus.OK);
+        Map<String, String> logoutDetails = new HashMap<>();
+        logoutDetails.put("logoutUrl", logoutUrl);
+        logoutDetails.put("idToken", idToken.getTokenValue());
+        request.getSession(false).invalidate();
+        return ResponseEntity.ok().body(logoutDetails);
     }
 }
