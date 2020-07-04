6bde346eaee182d48c39668971645488941bb4aa
Antonio Lorusso <mitictonylor@gmail.com>
2020-07-04 16:10:36 +0100

    add

--- src/main/java/EatInGlasgow/Application.java
+++ src/main/java/EatInGlasgow/Application.java
@@ -2,12 +2,20 @@ package EatInGlasgow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	@GetMapping("/hello-auth")
+	public String hello(@AuthenticationPrincipal OidcUser user) {
+		return "Hello, " + user.getFullName();
+	}
 }
