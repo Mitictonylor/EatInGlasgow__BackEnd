04411efcedf1b45176b54b1c90cbec5d83fe26c6
Antonio Lorusso <mitictonylor@gmail.com>
2020-07-04 15:13:37 +0100

    add mapping for all okta users

--- src/main/java/EatInGlasgow/controllers/AdminController.java
+++ src/main/java/EatInGlasgow/controllers/AdminController.java
@@ -1,4 +1,23 @@
 package EatInGlasgow.controllers;
 
+
+import com.okta.sdk.client.Client;
+import com.okta.sdk.resource.user.UserList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+    @Autowired
+    public Client client;
+
+
+    @GetMapping("/users")
+    public UserList getUsers() {
+        return client.listUsers();
+    }
+
+
+
 }
